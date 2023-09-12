dc3e0abd580061c8cce9532aa6bddbbac442bb0f
Connor Sicheri <Connor@Frank-MacBook-Pro.local>
2023-09-11 22:45:15 -0400

    Completed except for testing

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int step_count = 0;
+        while (n != 0){
+            if (n % 2 == 0){
+                n = n/2;
+            }
+            else{
+                n -= 1;
+            }
+            step_count += 1;
+        }
+        System.out.println(step_count);
+    }
 }
